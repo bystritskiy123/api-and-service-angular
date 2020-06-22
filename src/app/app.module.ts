0d5eaebd8455ee4171f2a1953efdbedc6e2a5f48
bystritskiy123 <gor_ell@mail.ru>
2020-06-22 16:11:02 +0300

    api and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule}  from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { ApiServiceService } from './api-service.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
